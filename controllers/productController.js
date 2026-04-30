ebfb198f0cc8081bf83ce8fa291a711fcaf79089
anamkapadia07 <anamkapadia07@gmail.com>
2026-05-01 01:33:22 +0500

    backend

---
+++ controllers/productController.js
@@ -0,0 +1,11 @@
+import Product from "../models/Product.js";
+
+// GET all products
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ models/Product.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  description: String,
+  category: String
+});
+
+export default mongoose.model("Product", productSchema);
